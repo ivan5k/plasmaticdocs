99ec6ca407899f40034e9fe54f2f2411676b5556
Ivan Inyushin <inushin_ivan@hotmail.com>
2018-10-29 14:25:39 +0200

    Added docker support

---
+++ dockerbuild.cmd
@@ -0,0 +1,5 @@
+# First instlal Docker for Windows
+
+docker pull keimlink/sphinx-doc
+
+docker run -it --rm -v "D:\Ivan\Plasmatron\plasmaticdocs":/home/python/docs keimlink/sphinx-doc:1.7.1 make -C docs html
